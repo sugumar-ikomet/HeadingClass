af83c694b07103d899ac73d5f7950e795b951be3
sugumar venkatesan <sugumar.v@ikomet.com>
2020-11-07 13:54:57 +0530

    file upload and processing and download

--- codes/config.py
+++ codes/config.py
@@ -1,14 +1,23 @@
+import os
+
 # directory to read raw html files from.Please make sure that only html files are present
 # in the folder. No duplicate names for files.
-raw_dir ='/home/sysadmin/h1_h6_heading/final_files/final_files/raw_html/'
+raw_dir ='/home/sysadmin/Desktop/MYCOBE/h1_h6_heading/final_files/final_files/raw_html/'
 # directory to store corrected HTML files
-rewrite_dir = '/home/sysadmin/h1_h6_heading/final_files/final_files/structured_html/'
+rewrite_dir = '/home/sysadmin/Desktop/MYCOBE/h1_h6_heading/final_files/final_files/structured_html/'
 # directory to save intermediate files
-temp_dir = "/home/sysadmin/h1_h6_heading/final_files/final_files/temp/"
+temp_dir = "/home/sysadmin/Desktop/MYCOBE/h1_h6_heading/final_files/final_files/temp/"
 # directory where nltk models are kept
-nltk_dir = "/home/sysadmin/h1_h6_heading/final_files/final_files/nltk/"
+nltk_dir = "/home/sysadmin/Desktop/MYCOBE/h1_h6_heading/final_files/final_files/nltk/"
 # directory where model objects etc... are kept
-model_objects_dir = "/home/sysadmin/h1_h6_heading/final_files/final_files/model_objects/"
+model_objects_dir = "/home/sysadmin/Desktop/MYCOBE/h1_h6_heading/final_files/final_files/model_objects/"
+
+remote_path = os.path.join('/var','www','html','CobeFileServer','Prod')
+
+remote_host = '192.168.1.3'
+remote_user = 'cobe'
+remote_pass = 'ikomet15'
+remote_port = '22'
 
 # names of files kept in 'model_objects_dir'
 # Do not change unless you changed the name of files.
--- codes/main.py
+++ codes/main.py
@@ -9,42 +9,64 @@ from classification import *
 from rules_final import *
 from rewrite_html import *
 from bs4 import BeautifulSoup
-
+import paramiko
+import base64
 #raw_dir = "D:/ML_Cobe/ikomet/scoring/raw_html/JTD_TestData6_HTMLCleanup.html"
 #HtmlFile = open(raw_dir, 'r', encoding='utf-8')
 #source_code = HtmlFile.read()
 
 from flask import request
-from flask import Flask, render_template, redirect, url_for, make_response
+from flask import Flask, render_template, redirect, url_for, jsonify, make_response
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/heading/detection', methods=['POST'])
 
 def h1_h6_heading_detection():
+    try:
+        print('original request',request);
+        req = request.get_json();
+        
+        print('after jsonify',req);
+        # return make_response(jsonify({"error":None}),200)
 
-    req = request.get_json()
-    #print(req)
-    #print(req['rawfilepath'])
-    #raw_dir = req['rawfilepath']
-    #print(req['rewrite'])
-    #rewrite_dir = req['rewrite']
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(remote_host, remote_port, remote_user, remote_pass)
+        sftp_client = ssh_client.open_sftp()
+        remote_file_path = os.path.join(remote_path,req['projectName'],req['workObjectID'],'HTML',req['fileName'])
+        print('remote_file_path:',remote_file_path)
+        remote_file = sftp_client.open(remote_file_path)
 
-    df1 = x_prep(raw_dir)
-    df1.to_csv(temp_dir+'temp1.csv',index=False)
-    df1 = pd.read_csv(temp_dir+"temp1.csv")
-    df2 = feature_create(df1)
-    df2.to_csv(temp_dir+'temp2.csv',index=False)
-    df2 = pd.read_csv(temp_dir+"temp2.csv")
-    df3 = lag_lead_feature(df2)
-    df3.to_csv(temp_dir+'temp3.csv',index=False)
-    df3 = pd.read_csv(temp_dir+"temp3.csv")
-    df4 = model_classification(df3)
-    df4.to_csv(temp_dir+'temp4.csv',index=False)
-    df4 = pd.read_csv(temp_dir+"temp4.csv")
-    df5 = rule_final(df4)
-    df5.to_csv(temp_dir+"final_output.csv",index=False)
-    html_create(df5)
+        print(req);
+
+        f = open(raw_dir+'/'+req['fileName'], "wb")
+        f.write(remote_file.read())
+        f.close()
+        remote_file.close()
+
+
+        df1 = x_prep(raw_dir)
+        df1.to_csv(temp_dir+'temp1.csv',index=False)
+        df1 = pd.read_csv(temp_dir+"temp1.csv")
+        df2 = feature_create(df1)
+        df2.to_csv(temp_dir+'temp2.csv',index=False)
+        df2 = pd.read_csv(temp_dir+"temp2.csv")
+        df3 = lag_lead_feature(df2)
+        df3.to_csv(temp_dir+'temp3.csv',index=False)
+        df3 = pd.read_csv(temp_dir+"temp3.csv")
+        df4 = model_classification(df3)
+        df4.to_csv(temp_dir+'temp4.csv',index=False)
+        df4 = pd.read_csv(temp_dir+"temp4.csv")
+        df5 = rule_final(df4)
+        df5.to_csv(temp_dir+"final_output.csv",index=False)
+        html_create(df5)
+        sftp_client.put(rewrite_dir+req['fileName'],remote_file_path);
+        print('success');
+        return make_response(jsonify({"error":None}),200)
+    except Exception as e:
+        print(e);
+        return make_response(jsonify({"error":e}),500)
     #print(df5)
     #soup = BeautifulSoup(df5, 'lxml')
     #return soup
